15cf81c16ab8ef93379b0a01c0ec1a145fe03603
Flavio Henrique <flavioh825@gmail.com>
2019-05-06 11:29:46 -0300

    Botão mobile.

--- src/components/Header/ButtomMob.js
+++ src/components/Header/ButtomMob.js
@@ -1,11 +1,30 @@
 import React from 'react';
+import { FaBars } from "react-icons/fa"
 
 class ButtomMob extends React.Component {
+
+  state = {
+    visNav: 'flex'
+  }
+
+  clickMe = () => {
+    if(this.state.visNav === 'flex') {
+      this.setState({
+        visNav: 'none'
+      });
+    } else {
+      this.setState({
+        visNav: 'flex'
+      });
+    }
+    document.querySelector(".nav").style.display = this.state.visNav;
+  }
+
   render() {
     return(
-      <>
-        <h1>ButtomMob</h1>
-      </>
+      <div className="buttonMob" onClick={ this.clickMe.bind(this) } >
+        <FaBars />
+      </div>
     );
   }
 }
